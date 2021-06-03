b0a5e3e36bdc47cc3f9fa7fed77a101c36037f65
Madhukanth T <madhu051196@gmail.com>
2021-06-03 15:19:14 +0530

    Added 309

--- 309.best_time_to_buy_and_sell_stock_with_cooldown.js
+++ 309.best_time_to_buy_and_sell_stock_with_cooldown.js
@@ -2,21 +2,36 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function (prices) {
-  const n = prices.length;
-  let empty = new Array(n);
-  let sold = new Array(n);
-  let hold = new Array(n);
+var maxProfit = function (P) {
+  const len = P.length;
 
-  empty[0] = 0;
-  sold[0] = 0;
-  hold[0] = -prices[0];
+  if (len === 0 || len === 1) return 0;
 
-  for (let i = 1; i < n; i++) {
-    empty[i] = Math.max(empty[i - 1], sold[i - 1]);
-    hold[i] = Math.max(hold[i - 1], empty[i - 1] - prices[i]);
-    sold[i] = Math.max(sold[i - 1], hold[i - 1] + prices[i]);
+  if (len === 2 && P[1] > P[0]) return P[1] - P[0];
+
+  if (len === 2 && P[0] >= P[1]) return 0;
+
+  const dp = new Array(len).fill().map(() => new Array(2).fill(0));
+
+  // 0th day, I have no stock
+  dp[0][0] = 0;
+
+  // 0th day, I have stock
+  dp[0][1] = -P[0];
+
+  // 1st day, I have no stock
+  dp[1][0] = Math.max(dp[0][0], dp[0][1] + P[1]);
+
+  // 1st day, I have stock
+  dp[1][1] = Math.max(dp[0][1], dp[0][0] - P[1]);
+
+  for (let i = 2; i < len; i++) {
+    // I have no stock on i th day, either I am carry forwarding with no stock or i sold on i th day
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + P[i]);
+
+    // I have stock on i th day, either I am carryforwarding with stock or i bought the stock on i th day
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - P[i]);
   }
 
-  return Math.max(empty.pop(), sold.pop());
+  return dp[len - 1][0];
 };
